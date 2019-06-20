a12f79c4da728fb16b8649cf7c7e498abc7f6df3
Dongha Kang <cleardh@hotmail.com>
2019-06-20 00:08:26 -0400

    Added authentication

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 const db = require('../models');
 
 exports.register = async (req, res, next) => {
@@ -5,8 +7,14 @@ exports.register = async (req, res, next) => {
     try {
         const user = await db.User.create(req.body);
         const {id, username} = user;
-        res.json({id, username});
+
+        const token = jwt.sign({id, username}, process.env.secret);
+
+        res.status(201).json({id, username, token});
     } catch(err) {
+        if(err.code === 11000) {
+            err.message = 'Sorry, that username is already taken';
+        }
         next(err);
     }
 }
@@ -16,12 +24,16 @@ exports.login = async (req, res, next) => {
         const user = await db.User.findOne({username: req.body.username});
         const {id, username} = user;
         const valid = await user.comparePassword(req.body.password);
+        
         if(valid) {
-            res.json({id, username});
+            const token = jwt.sign({id, username}, process.env.secret);
+
+            res.json({id, username, token});
         } else {
-            throw new Error('Invalid username/password');
+            throw new Error();
         }
     } catch(err) {
+        err.message = 'Invalid username/password';
         next(err);
     }
 }
